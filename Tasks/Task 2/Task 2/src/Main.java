221aa664c6b52ef553f443fa5cf86134145c6009
John Emil <156576017+john1909m@users.noreply.github.com>
2025-07-23 11:25:33 +0300

    Added 2 problems sheet asyout

--- Tasks/Task 2/Task 2/src/Main.java
+++ Tasks/Task 2/Task 2/src/Main.java
@@ -15,6 +15,40 @@ public class Main {
             e.work();
             System.out.println("----------------------------");
         }
+ //---------------------Sheet Asyout-------------------------//
+
+        //-----------------------------------------------O----------------------------------------//
+
+//        Scanner scanner=new Scanner(System.in);
+//        int num1=scanner.nextInt();
+//        char op=scanner.next().charAt(0);
+//        int num2=scanner.nextInt();
+//        if(op=='+'){
+//            System.out.println(num1+num2);
+//        }else if(op=='-'){
+//            System.out.println(num1-num2);
+//        } else if(op=='*'){
+//            System.out.println(num1*num2);
+//        } else if(op=='/'){
+//            if(num2!=0){
+//                System.out.println(num1/num2);
+//            }else{
+//                System.out.println("Cannot divide by zero");
+//            }
+//        }
+
+
+
+        //-----------------------------------------------P----------------------------------------//
+
+//        Scanner scanner=new Scanner(System.in);
+//        char number=scanner.next().charAt(0);
+//        int num=(int)number;
+//        if(num%2==0){
+//            System.out.println("Even");
+//        } else if (num%2!=0) {
+//            System.out.println("Odd");
+//        }
 
 
     }
