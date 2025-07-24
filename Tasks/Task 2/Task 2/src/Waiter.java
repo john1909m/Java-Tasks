5d1d2977e21aecb9e038f8bce88c14f622746903
John Emil <156576017+john1909m@users.noreply.github.com>
2025-07-25 01:34:03 +0300

    Update online oop 2

--- Tasks/Task 2/Task 2/src/Waiter.java
+++ Tasks/Task 2/Task 2/src/Waiter.java
@@ -1,9 +1,13 @@
-public class Waiter extends Employee {
+public class Waiter extends Employee implements Eatable {
     public Waiter(String name, int age,double salary) {
         super(name,age,salary,"Waiter");
     }
     @Override
     public void work() {
         System.out.println("Waiter "+ getName() + " is serving customers");
+    }
+    @Override
+    public void eat() {
+        System.out.println("Waiter " + getName() + " is having a quick meal break.");
     }
 }
