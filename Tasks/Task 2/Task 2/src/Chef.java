119e31f3024e0cc2a3f139be48473227aa1b129e
John Emil <156576017+john1909m@users.noreply.github.com>
2025-07-25 01:33:36 +0300

    Update online oop 2

--- Tasks/Task 2/Task 2/src/Chef.java
+++ Tasks/Task 2/Task 2/src/Chef.java
@@ -1,7 +1,12 @@
-public class Chef extends Employee {
+public class Chef extends Employee implements Eatable {
 
     public Chef(String name, int age, double salary) {
         super(name, age, salary, "Chef");
+    }
+    @Override
+    public void eat() {
+        System.out.println("Chef "+ getName() + " is tasting the food.");
+
     }
 
     @Override
